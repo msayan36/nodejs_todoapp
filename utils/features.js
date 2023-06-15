99df8155bc279dabf54804a13ceedad4f19f72c1
Sayan Munshi <sayanmunshi36@gmail.com>
2023-06-15 11:46:04 +0530

    Update features.js

--- utils/features.js
+++ utils/features.js
@@ -8,7 +8,7 @@ export const sendCookie = (user, res, message, statusCode = 200) => {
     .cookie("token", token, {
       httpOnly: true,
       maxAge: 15 * 60 * 1000,
-      sameSite: process.env.NODE_ENV === "development" ? "lax" : none,
+      sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
       secure: process.env.NODE_ENV === "development" ? false : true,
     })
     .json({
